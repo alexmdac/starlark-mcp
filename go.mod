fedac8eff13432a0aa7f7cfee7532d869934a181
Alex Mendes da Costa <alexmdac@gmail.com>
2025-09-15 01:44:18 -0700

    Starlark MCP server

--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/alexmdac/starlark-mcp
 
 go 1.25.1
 
-require go.starlark.net v0.0.0-20250906160240-bf296ed553ea
+require (
+	github.com/modelcontextprotocol/go-sdk v0.5.0
+	go.starlark.net v0.0.0-20250906160240-bf296ed553ea
+)
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	github.com/google/jsonschema-go v0.2.3 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,23 +1,74 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"fmt"
 	"log"
 
 	"go.starlark.net/starlark"
 	"go.starlark.net/syntax"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
 )
 
-func main() {
-	program := `print("Hello, World!")`
+type ExecuteStarlarkParams struct {
+	Program string `json:"program" jsonschema:"a valid Starlark program"`
+}
 
-	thread := &starlark.Thread{}
-	_, err := starlark.EvalOptions(
+func ExecuteStarlark(
+	ctx context.Context,
+	req *mcp.CallToolRequest,
+	args ExecuteStarlarkParams,
+) (*mcp.CallToolResult, any, error) {
+	var buf bytes.Buffer
+	thread := &starlark.Thread{
+		Print: func(thread *starlark.Thread, msg string) {
+			buf.WriteString(msg) // This panics on OOM, never returns a non-nil error.
+			buf.WriteRune('\n')
+		},
+	}
+	_, err := starlark.ExecFileOptions(
 		syntax.LegacyFileOptions(),
 		thread,
-		"built-in program",
-		program,
+		"LLM supplied program",
+		args.Program,
 		nil)
 	if err != nil {
+		return nil, nil, fmt.Errorf("failed to execute program: %v", err)
+	}
+	return &mcp.CallToolResult{
+		Content: []mcp.Content{
+			&mcp.TextContent{Text: buf.String()},
+		},
+	}, nil, nil
+}
+
+const ExecuteStarlarkDescription = `Executes Starlark programs.
+
+See https://raw.githubusercontent.com/google/starlark-go/bf296ed553ea1715656054a7f64ac6a6dd161360/doc/spec.md
+for the Starlark language specification.
+
+Programs can emit output using the Starlark "print" statement. The "print" statement
+outputs the given string with a newline added. The MCP server returns the aggregated
+output.`
+
+var ExecuteStarlarkTool = &mcp.Tool{
+	Name:        "ExecuteStarlark",
+	Description: ExecuteStarlarkDescription,
+}
+
+const ServerName = "StarlarkMCP"
+
+func RunMCPServer(ctx context.Context) error {
+	server := mcp.NewServer(&mcp.Implementation{Name: ServerName}, nil)
+	mcp.AddTool(server, ExecuteStarlarkTool, ExecuteStarlark)
+	return server.Run(ctx, &mcp.StdioTransport{})
+}
+
+func main() {
+	ctx := context.Background()
+	if err := RunMCPServer(ctx); err != nil {
 		log.Fatal(err)
 	}
 }
